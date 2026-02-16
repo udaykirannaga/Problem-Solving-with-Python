38ef48d7a85f64d90c083cd16383705ae4b00df3
udaykirannaga <nagauday7788@gmail.com>
2026-02-16 12:15:50 +0530

    Day-16 commit

---
+++ Day-16/patterns.py
@@ -0,0 +1,60 @@
+''' 
+write a python code print patterns
+
+
+* * * *
+* * * *
+* * * *
+* * * *
+'''
+
+for i in range(1,5):
+    for j in range(1,5):
+        print("*",end=" ")
+    print()
+
+
+'''
+*
+* *
+* * *
+* * * *
+
+'''
+
+print("half pyramid")
+for i in range(1,5)    :
+    print(i*"* ")
+ # or
+
+for i in range(1,5):
+    for j in range(i):
+        print("# ", end="")
+    print()
+
+'''
+# Reverse half pyramid
+* * * *
+* * *
+* *
+*
+'''
+
+for i in range(4):
+    for j in range(4-i):
+        print("# ",end="")
+    print()
+
+'''
+1 2 3 4
+2 3 4
+3 4 
+4
+
+'''
+
+n=4
+for i in range(1,n+1):
+    for j in range(i,n+1):
+        print(j,end=" ")
+    print()
