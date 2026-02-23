a3c487c29b9b54f09bbee1e362f6b512ccffa0de
udaykirannaga <nagauday7788@gmail.com>
2026-02-23 12:15:01 +0530

    Day-18 commit

---
+++ Day-18/gen_Random_num.py
@@ -0,0 +1,8 @@
+'''
+Write a Numpy program to generate a random integers from from 1 to 20
+'''
+
+import numpy as np
+# num=np.random.randint(1,10)
+num=np.random.randint(low=1,high=50,size=10)
+print(num)
