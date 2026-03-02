0c52756f5d9ccc06bd6f77d12371aa01ee3e3dfa
udaykirannaga <nagauday7788@gmail.com>
2026-03-02 12:51:24 +0530

    Day-24 commit

---
+++ Day-24/a_z.py
@@ -0,0 +1,9 @@
+'''
+write a python program to print alphabets a -z
+
+'''
+# import string module to get char
+import string
+# using loop for iteration
+for letters in string.ascii_lowercase:
+    print(letters,end=" ")
